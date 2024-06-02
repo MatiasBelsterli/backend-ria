606801253589c4242debdb9768ab9d7d659c4a00
Tiago Silva <tiaval@hotmail.es>
2024-06-02 05:20:20 -0300

    add endpoints to baker-order, and comment pre exist orders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const ordersController = require('../controllers/ordersController');
-const { verifyToken, isAdmin, isUser } = require('../middleware/auth');
+const { verifyToken, isAdmin, isUser, isBaker } = require('../middleware/auth');
 
 router.get('/', verifyToken, isUser, (req, res) => {
     /* #swagger.summary = 'Gets the list of orders' */
     /* #swagger.tags = ['Orders'] */
     ordersController.getOrders(req, res);
+});
+
+router.get('/bakers', verifyToken, isBaker, (req, res) => {
+    /* #swagger.summary = 'Gets the list of orders to bakers' */
+    /* #swagger.tags = ['Orders'] */
+    ordersController.getOrdersToBakers(req, res);
+});
+
+router.patch('/bakers/:id', verifyToken, isBaker, (req, res) => {
+    /* #swagger.summary = 'Accepts an order' */
+    /* #swagger.tags = ['Orders'] */
+    ordersController.takeOrderToBaker(req, res);
+});
+
+router.get('/baker', verifyToken, isBaker, (req, res) => {
+    /* #swagger.summary = 'Gets an order to bakers by ID' */
+    /* #swagger.tags = ['Orders'] */
+    ordersController.getOrdersByBaker(req, res);
 });
 
 router.get('/:id', verifyToken, isUser, (req, res) => {
